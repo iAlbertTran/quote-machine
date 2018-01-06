4e9a12f4c73b0d74fbd9c6687b3a73cb92b58d8d
A-Mac <albtran@ucdavis.edu>
2018-01-06 00:06:22 -0800

    substantial changes to layout, and how the content is generated via JS

--- quote-machine.js
+++ quote-machine.js
@@ -3,26 +3,52 @@ var URL = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[p
 
 //sets up the quote box on page load
 window.onload = function setUp(){
+
+	var quoteBox = document.getElementById("quote-box");
+	var rect = quoteBox.getBoundingClientRect();
+	var arrow = document.getElementById("arrow");
+	arrow.style.top = rect.bottom + "px";
+	arrow.style.left = rect.left + "px";
+
 	var script = document.createElement("script");
 	script.type = 'text/javascript';
 	script.src = URL + "&" + Math.random().toString(16);
 	document.body.appendChild(script);
+	twitterButton();
 };
 
+//adjust the position of the speach bubble arrow
+window.onresize = function adjust(){
+	var quoteBox = document.getElementById("quote-box");
+	var rect = quoteBox.getBoundingClientRect();
+	var arrow = document.getElementById("arrow");
+
+	//minus 5 and plus 8 px respectively to get the arrow at the very bottom right corner 
+	arrow.style.top = rect.bottom + "px";
+	arrow.style.left = rect.left + "px";
+}
 
 //callback function from JSONP
 function setQuote(data){
-	console.log(data);
 	var quoteBox = document.getElementById("quote-box");
 	var quote = document.createElement("div");
 	quote.id = "quote";
-	quote.innerHTML = data[0].content;
-	quoteBox.appendChild(quote);
+
+	var string = data[0].content.split("");
+	//replace "<p>" at the beginning and end of the string with "<em>" and double quotes.
+	string.splice(1,2,"em>\"");
+	string.splice(string.length-5, 3, "\"<em");
+	quote.innerHTML = string.join("");
+
+	//insert quote in middle
+	var tweetButton = document.getElementById("tweet");
+	quoteBox.insertBefore(quote, tweetButton);
+
+	//creates div for name of author behind quote
 	var author = document.createElement("div");
 	author.id = "author";
 	author.innerHTML = "- " + data[0].title;
 	quote.appendChild(author);
-	twitterButton();
 }
 
 //creates the tweet button
@@ -32,7 +58,8 @@ function twitterButton(){
 	tweet.id = "tweet";
 	tweet.classList.add("fa");
 	tweet.classList.add("fa-twitter");
-	quoteBox.appendChild(tweet);
+	tweet.classList.add("fa-2x");
+	quoteBox.parentNode.insertBefore(tweet, quoteBox.nextSibling);
 }
 
 
@@ -42,14 +69,8 @@ function generateQuote(){
 	document.body.removeChild(document.body.lastChild);
 
 	var quoteBox = document.getElementById("quote-box");
-	quoteBox.innerHTML = "";
-
-	var button = document.createElement("button");
-	button.id = "random";
-	button.setAttribute("type", "button");
-	button.setAttribute("onClick", "generateQuote()");
-	button.innerHTML = "Randomize";
-	quoteBox.appendChild(button);
+	quoteBox.removeChild(document.getElementById("quote"));
+    
 
 	var script = document.createElement("script");
 	script.type = 'text/javascript';
